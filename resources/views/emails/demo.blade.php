f74b408038a4c7be346b2addbf56f576269079bc
rkemmy <rkemmy.rk69@gmail.com>
2018-08-30 10:30:47 +0300

    create a function to render the demo mail

---
+++ resources/views/emails/demo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+ 
+<body>
+<h2>Welcome to the site</h2>
+<br/>
+This is an email demo from proposal app
+</body>
+ 
+</html>
